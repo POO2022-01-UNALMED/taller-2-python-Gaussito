7d7c9c0663f8a1c2b8595be499648e3de9781609
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-03-21 21:34:20 -0500

    Primer Commit

---
+++ main.py
@@ -0,0 +1,49 @@
+class Asiento:
+
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
+            self.color = color
+
+
+class Motor:
+
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        self.tipo = tipo
+
+
+class Auto:
+
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = []
+        self.asientos.append(asientos)
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Auto.cantidadCreados += 1
+
+    @classmethod
+    def cantidadAsientos(cls):
+        return cls.cantidadCreados
+
+    def verificarIntegridad(self):
+        for asiento in self.asientos:
+            if not (asiento.registro == self.motor.registro == self.registro):
+                return "Las piezas no son originales"
+        return "Auto original"
